0cae7776e41bae70e0b95966e9059d8a38819142
ShervinDani <rvshervin@gmil.com>
2025-07-11 19:29:48 +0530

    Server and ApiGateway Added

---
+++ flexo_apigateway/src/test/java/com/apigateway/backend/FlexoApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexoApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ flexo_server/src/main/java/com/server/backend/FlexoServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexoServerApplication.class, args);
+	}
+
+}

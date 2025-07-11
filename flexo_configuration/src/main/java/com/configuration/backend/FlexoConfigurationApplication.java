0ddba738073b303a43b27fc03bbe792d72153e27
ShervinDani <rvshervin@gmil.com>
2025-07-11 19:35:17 +0530

    Users and COnfigurations added

---
+++ flexo_configuration/src/main/java/com/configuration/backend/FlexoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.configuration.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexoConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexoConfigurationApplication.class, args);
+	}
+
+}
---
+++ flexo_configuration/src/test/java/com/configuration/backend/FlexoConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configuration.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexoConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
